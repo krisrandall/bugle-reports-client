f7c66f1cb89ca364a218b19f23645c259c5ea7d6
Barry Earsman <barryearsman@gmail.com>
2016-12-16 06:12:50 +0000

    Added SDK

---
+++ sdk/test.js
@@ -0,0 +1,30 @@
+var Sdk = require('./newnrg-reporter-sdk.js');
+
+var client = new Sdk.Client({host:'45.79.83.93', port: 8081});
+client.createDataType('questionnaire2', function(err, data) {
+    if (err) {
+        console.error(err);
+    }
+    else {
+        console.log(JSON.stringify(data));
+    }
+})
+
+client.addRecord('questionnaire', 
+    {
+        "program_id": "ltwl",
+        "custodian": "partner1",
+        "contributor": "baz", 
+        "item_id": "favourite_things",
+        "action": "start",
+        "overall_start": 0
+    },
+    function(err, data) {
+        if (err) {
+            console.error(err);
+        }
+        else {
+            console.log(JSON.stringify(data));
+        }       
+    }
+)
